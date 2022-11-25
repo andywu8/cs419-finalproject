58e0c2c1ce5ac7e8f2f07680265c46d5c2d00b9e
Kishan Patel <patel.kishan.kbp@gmail.com>
2022-11-24 23:23:38 -0500

    find friends feature added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """main file to run flask app"""
 
 from flask import Flask, render_template, request, redirect, url_for
-from models import login, insert_friend, retrieve_potential_friends, retrieve_users, check_user_exists, insert_user, edit_profile_info
+from models import login, insert_friend, retrieve_potential_friends, retrieve_users, check_user_exists, insert_user, edit_profile_info, add_friend, get_my_friends, get_potential_matches
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -12,7 +13,7 @@ def home():
     users = None
     confirmation_message = None
 
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
@@ -23,37 +24,41 @@ def home():
         else:
             error_messages.append("Incorrect username or password")
             return render_template('index.html',
-                                   users = users,
-                                   error_messages = error_messages,
-                                   confirmation_message = confirmation_message)
+                                   users=users,
+                                   error_messages=error_messages,
+                                   confirmation_message=confirmation_message)
     else:
         return render_template('index.html')
+
 
 @app.route('/profile/<username>', methods=['POST', 'GET'])
 def profile(username):
     """profile page"""
     return render_template('profile.html', username=username)
 
+
 @app.route('/dashboard/<username>', methods=['POST', 'GET'])
 def dashboard(username):
     """dashboard page"""
-    if request.method=='POST':
+    if request.method == 'POST':
         residential_college = request.form.get('residential_college')
         class_year = request.form.get('class_year')
         gender = request.form.get('gender')
         orientation = request.form.get('orientation')
         match_preference = request.form.get('match_preference')
 
-        edit_profile_info(username, residential_college, class_year, gender, orientation, match_preference)
+        edit_profile_info(username, residential_college,
+                          class_year, gender, orientation, match_preference)
 
     return render_template('dashboard.html', username=username)
 
-@app.route('/signup', methods=['POST','GET'])
+
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
     """signup page"""
     error_messages = []
 
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         firstname = request.form['firstname']
@@ -68,29 +73,53 @@ def signup():
             error_messages.append("Username already exists")
 
     users = retrieve_users()
-    return render_template('signup.html', users = users, error_messages = error_messages)
+    return render_template('signup.html', users=users, error_messages=error_messages)
 
-@app.route('/find_friends/<username>', methods=['POST','GET'])
+
+@app.route('/find_friends/<username>', methods=['POST', 'GET'])
 def find_friends(username):
     """add friends page"""
     return render_template('find_friends.html', username=username)
 
-@app.route('/add_friends/<username>', methods=['POST','GET'])
+
+@app.route('/add_friends/<username>', methods=['POST', 'GET'])
 def add_friends(username):
     """add friends page"""
-    residential_college = request.args.get('residential_college')
-    class_year = request.args.get('class_year')
-    gender = request.args.get('gender')
-    orientation = request.args.get('orientation')
+    if request.method == 'GET':
+        residential_college = request.args.get('residential_college')
+        class_year = request.args.get('class_year')
+        gender = request.args.get('gender')
+        orientation = request.args.get('orientation')
 
-    potential_friends = retrieve_potential_friends(username, residential_college, class_year, gender, orientation)
-    return render_template('add_friends.html', username=username, potential_friends=potential_friends)
+        potential_friends = retrieve_potential_friends(
+            username, residential_college, class_year, gender, orientation)
+        return render_template('add_friends.html', username=username, potential_friends=potential_friends)
+    else:
+        friend_username = request.args.get('friend_username')
+        add_friend(username, friend_username)
+        return render_template('add_friends.html', username=username)
 
-@app.route('/inbox', methods=['POST','GET'])
+
+@app.route('/inbox', methods=['POST', 'GET'])
 def inbox():
     """inbox page"""
     return render_template('inbox.html')
 
+
+@app.route('/match/<username>', methods=['POST', 'GET'])
+def match(username):
+    """match page"""
+    if request.method == "GET":
+        my_friends = get_my_friends(username)
+        return render_template('match.html', username=username, my_friends=my_friends)
+    else:
+        my_friends = get_my_friends(username)
+        friend_username = request.args.get('friend_username')
+        potential_matches = get_potential_matches(friend_username)
+        print(potential_matches)
+        return render_template('match.html', username=username, my_friends=my_friends)
+
+
 if __name__ == '__main__':
     """runs the application on a server"""
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port=8000)
--- models.py
+++ models.py
@@ -1,118 +1,161 @@
 import sqlite3 as sql
 
+
 def login(username, password):
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	query = "SELECT count(*) FROM users WHERE username = ? AND password = ?"
-	cur.execute(query, [username, password])
-	(num_users_with_username_and_password,) = cur.fetchone()
-	con.commit()
-	con.close()
-	if num_users_with_username_and_password > 0:
-		return True
-	else:
-		return False
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "SELECT count(*) FROM users WHERE username = ? AND password = ?"
+    cur.execute(query, [username, password])
+    (num_users_with_username_and_password,) = cur.fetchone()
+    con.commit()
+    con.close()
+    if num_users_with_username_and_password > 0:
+        return True
+    else:
+        return False
+
 
 def insert_friend(username, friend):
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	query = "INSERT INTO friends (username, friend) VALUES (?, ?)"
-	cur.execute(query, (username, friend))
-	con.commit()
-	con.close()
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "INSERT INTO friends (username, friend) VALUES (?, ?)"
+    cur.execute(query, (username, friend))
+    con.commit()
+    con.close()
+
 
 def retrieve_potential_friends(username, residential_college, class_year, gender, orientation):
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	args = []
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    args = []
 
-	if residential_college == 'any':
-		residential_college = None
-	if class_year == 'any':
-		class_year = None
-	if gender == 'any':
-		gender = None
-	if orientation == 'any':
-		orientation = None
+    if residential_college == 'any':
+        residential_college = None
+    if class_year == 'any':
+        class_year = None
+    if gender == 'any':
+        gender = None
+    if orientation == 'any':
+        orientation = None
 
-	query = "SELECT id, firstname, lastname FROM users "
-	if residential_college or class_year or gender or orientation:
-		query += "WHERE "
-		if residential_college:
-			query += "college = ? "
-			args.append(residential_college)
-			if class_year or gender or orientation:
-				query += "AND "
-		if class_year:
-			query += "class_year = ? "
-			args.append(class_year)
-			if gender or orientation:
-				query += "AND "
-		if gender:
-			query += "gender = ? "
-			args.append(gender)
-			if orientation:
-				query += "AND "
-		if orientation:
-			query += "orientation = ? "
-			args.append(orientation)
-		query += "AND username != ? "
-	else:
-		query += "WHERE username != ? "
+    query = "SELECT username, firstname, lastname FROM users "
+    if residential_college or class_year or gender or orientation:
+        query += "WHERE "
+        if residential_college:
+            query += "college = ? "
+            args.append(residential_college)
+            if class_year or gender or orientation:
+                query += "AND "
+        if class_year:
+            query += "class_year = ? "
+            args.append(class_year)
+            if gender or orientation:
+                query += "AND "
+        if gender:
+            query += "gender = ? "
+            args.append(gender)
+            if orientation:
+                query += "AND "
+        if orientation:
+            query += "orientation = ? "
+            args.append(orientation)
+        query += "AND username != ? "
+    else:
+        query += "WHERE username != ? "
 
-	args.append(username)
-	cur.execute(query, args)
-	friends = []
-	row = cur.fetchone()
-	while row is not None:
-		friends.append([row[0], row[1], row[2]])
-		print(row)
-		row = cur.fetchone()
-	con.close()
-	return friends
+    args.append(username)
+    cur.execute(query, args)
+    friends = []
+    row = cur.fetchone()
+    while row is not None:
+        friends.append([row[0], row[1], row[2]])
+        print(row)
+        row = cur.fetchone()
+    con.close()
+    return friends
+
 
 def retrieve_usernames(user_username):
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	query = "SELECT username FROM users WHERE username = ?"
-	cur.execute(query, [user_username])
-	con.commit()
-	con.close()
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "SELECT username FROM users WHERE username = ?"
+    cur.execute(query, [user_username])
+    con.commit()
+    con.close()
+
 
 def check_user_exists(username):
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	query = "SELECT count(*) FROM users WHERE username = ?"
-	cur.execute(query, [username])
-	(num_users_with_username,) = cur.fetchone()
-	print(num_users_with_username)
-	con.commit()
-	con.close()
-	if num_users_with_username > 0:
-		return True
-	else:
-		return False
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "SELECT count(*) FROM users WHERE username = ?"
+    cur.execute(query, [username])
+    (num_users_with_username,) = cur.fetchone()
+    print(num_users_with_username)
+    con.commit()
+    con.close()
+    if num_users_with_username > 0:
+        return True
+    else:
+        return False
 
-def insert_user(firstname,lastname,username,password):
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	query = "INSERT INTO users (firstname, lastname, username, password) VALUES (?, ?, ?, ?)"
-	cur.execute(query, (firstname, lastname, username, password))
-	con.commit()
-	con.close()
+
+def insert_user(firstname, lastname, username, password):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "INSERT INTO users (firstname, lastname, username, password) VALUES (?, ?, ?, ?)"
+    cur.execute(query, (firstname, lastname, username, password))
+    con.commit()
+    con.close()
+
 
 def retrieve_users():
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	cur.execute("SELECT username FROM users")
-	users = cur.fetchall()
-	con.close()
-	return users
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT username FROM users")
+    users = cur.fetchall()
+    con.close()
+    return users
+
 
 def edit_profile_info(username, residential_college, class_year, gender, orientation, match_preference):
-	con = sql.connect("database.db")
-	cur = con.cursor()
-	query = "UPDATE users SET college = ?, class_year = ?, gender = ?, orientation = ?, preference = ? WHERE username = ?"
-	cur.execute(query,[residential_college, class_year, gender, orientation, match_preference, username])
-	con.commit()
-	con.close()
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "UPDATE users SET college = ?, class_year = ?, gender = ?, orientation = ?, preference = ? WHERE username = ?"
+    cur.execute(query, [residential_college, class_year,
+                gender, orientation, match_preference, username])
+    con.commit()
+    con.close()
+
+
+def add_friend(username, friend_username):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "INSERT INTO friends (username, friend) VALUES (?, ?)"
+    cur.execute(query, [username, friend_username])
+    con.commit()
+    con.close()
+
+
+def get_my_friends(username):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "SELECT firstname, lastname, username from users WHERE username IN (SELECT friend from friends WHERE username = ?)"
+    cur.execute(query, [username])
+    friends = []
+    row = cur.fetchone()
+    while row is not None:
+        friends.append([row[0], row[1], row[2]])
+        print(row)
+        row = cur.fetchone()
+    con.close()
+    return friends
+
+
+def get_potential_matches(friend_username):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    query = "SELECT firstname, lastname, username from users WHERE gender IN (SELECT preference from users WHERE username = ?)"
+    cur.execute(query, [friend_username])
+    matches = cur.fetchall()
+    con.close()
+    return matches
